2d49aa9045d5421a2f7a8ce00ce2c6b7bf31b5d3
Christian Maia <christiansoaresmaia@gmail.com>
2025-05-16 11:03:16 -0300

    Christian - Adicionei atributos a "SubTurma"

--- src/main/java/com/senai/projeto_catraca/model/turma/SubTurma.java
+++ src/main/java/com/senai/projeto_catraca/model/turma/SubTurma.java
@@ -1,4 +1,13 @@
 package com.senai.projeto_catraca.model.turma;
 
+import com.senai.projeto_catraca.model.curso.Semestre;
+import com.senai.projeto_catraca.model.usuario.aluno.Aluno;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class SubTurma {
+    String nome;
+    List<Aluno> aluno = new ArrayList<>();
+    List<Semestre> semestre = new ArrayList<>();
 }
