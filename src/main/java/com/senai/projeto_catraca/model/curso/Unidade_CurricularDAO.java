fd9636007a2eeaafa40a73b3a97788692777678c
Ana Luiza <ani.luiza31@gmail.com>
2025-05-16 10:51:27 -0300

    Commit

--- src/main/java/com/senai/projeto_catraca/model/curso/Unidade_CurricularDAO.java
+++ src/main/java/com/senai/projeto_catraca/model/curso/Unidade_CurricularDAO.java
@@ -1,7 +1,5 @@
 package com.senai.projeto_catraca.model.curso;
 
 public class Unidade_CurricularDAO {
-    public static void main(String[] args) {
-        System.out.println("Teste");
-    }
+
 }
