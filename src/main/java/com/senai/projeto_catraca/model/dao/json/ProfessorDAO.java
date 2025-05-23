8a7981f331abcab0baaab70aeed3d7ad10760091
Allan <allanlealluz@gmail.com>
2025-05-23 09:06:54 -0300

    Professor view part 1

--- src/main/java/com/senai/projeto_catraca/model/dao/json/ProfessorDAO.java
+++ src/main/java/com/senai/projeto_catraca/model/dao/json/ProfessorDAO.java
@@ -1,4 +1,76 @@
 package com.senai.projeto_catraca.model.dao.json;
 
+
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.Gson;
+import com.senai.projeto_catraca.model.usuario.Administrador;
+import com.senai.projeto_catraca.model.usuario.Professor;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.*;
+
 public class ProfessorDAO {
+    private final String caminho = "professores.json";
+    private final Gson gson = new Gson();
+    private final List<Professor> professores;
+
+    public ProfessorDAO(){
+        professores = carregar();
+    }
+
+    private List<Professor> carregar() {
+        try (FileReader reader = new FileReader(caminho)) {
+            Type listType = new TypeToken<List<Professor>>() {}.getType();
+            return gson.fromJson(reader, listType);
+        } catch (IOException e) {
+            return new ArrayList<>();
+        }
+    }
+
+    private void salvar(List<Professor> lista) {
+        try (FileWriter writer = new FileWriter(caminho)) {
+            gson.toJson(lista, writer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void inserir(Professor professor) {
+
+        int novoId = professores.stream().mapToInt(Professor::getId).max().orElse(0) + 1;
+        professor.setId(novoId);
+        professores.add(professor);
+        salvar(professores);
+    }
+
+    public void atualizar(Professor professor) {
+        for (int i = 0; i < professores.size(); i++) {
+            if (professores.get(i).getId() == professor.getId()) {
+                professores.set(i, professor);
+                break;
+            }
+        }
+        salvar(professores);
+    }
+
+    public void remover(int id) {
+        professores.removeIf(p -> p.getId() == id);
+        salvar(professores);
+    }
+
+    public Optional<Professor> buscarPorId(int id) {
+        return carregar().stream().filter(p -> p.getId() == id).findFirst();
+    }
+
+    public Optional<Professor> buscarPorLogin(String login) {
+        return professores.stream().filter(a -> a.getNome().equals(login)).findFirst();
+    }
+
+    public List<Professor> listarTodos() {
+        return professores;
+    }
 }
+
--- src/main/java/com/senai/projeto_catraca/view/ProfessorView.java
+++ src/main/java/com/senai/projeto_catraca/view/ProfessorView.java
@@ -1,28 +1,53 @@
 package com.senai.projeto_catraca.view;
 
+import com.senai.projeto_catraca.model.dao.json.ProfessorDAO;
+import com.senai.projeto_catraca.model.usuario.Professor;
+
 import java.util.Scanner;
 
 public class ProfessorView {
     public static void main(String[] args) {
         System.out.printf("Bem vindo");
         Scanner scanner = new Scanner(System.in);
+        ProfessorDAO professorDAO = new ProfessorDAO();
         executarMenu("""
                     ===== MENU PROFESSOR =====
-                    1. Gerenciar Horários
-                    2. Receber notificações de atraso
+                    1. Cadastrar professor
+                    2. Listar Professor
                     3. Deslogar
                     0. Sair
                     """,
                 opcao -> {
                     switch (opcao) {
-                        case "1" -> System.out.println("Conectar");
-                        case "2" -> System.out.println("Opção 2");
-                        case "3" -> System.out.println("Opção 3");
-                        case "0" -> {
-                            System.out.println("Saindo...");
-                            System.exit(0);
+                        case "1" -> {
+                            System.out.println("Digite o nome: ");
+                            String nome = scanner.nextLine();
+                            System.out.println("Digite uma senha: ");
+                            String senha = scanner.nextLine();
+                            System.out.println("Digite uma cpf: ");
+                            String cpf = scanner.nextLine();
+                            System.out.println("Digite um endereço: ");
+                            String endereco = scanner.nextLine();
+                            System.out.println("Digite um disciplina: ");
+                            String disciplina = scanner.nextLine();
+                            System.out.println("Digite uma telefone: ");
+                            String telefone = scanner.nextLine();
+                            Professor novoProfessor = new Professor(nome,senha,cpf,0,endereco,telefone,disciplina);
+                            professorDAO.inserir(novoProfessor);
+                            System.out.println("Professor cadastrado com sucesso!");
+                            System.out.println("Nome: " + novoProfessor.getNome());
+                            System.out.println("Disciplina: " + novoProfessor.getDisciplina());
+                            System.out.println("Endereço: " + endereco);
                         }
-                        default  -> System.out.println("Opção inválida.");
+                        case "2" -> {
+                            professorDAO.listarTodos();
+                        }
+                        case "3" -> {
+                            System.out.println("Remover");
+                        }
+                        default -> {
+                            System.out.println("Opção inválida!");
+                        }
                     }
                 });
     }
