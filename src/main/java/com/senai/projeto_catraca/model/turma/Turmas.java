c368daecf1d66fb6f5eb9c14a8d540f800ce3826
Allan <allanlealluz@gmail.com>
2025-05-23 10:22:43 -0300

    TURMA p1

--- src/main/java/com/senai/projeto_catraca/model/turma/Turmas.java
+++ src/main/java/com/senai/projeto_catraca/model/turma/Turmas.java
@@ -1,4 +1,70 @@
 package com.senai.projeto_catraca.model.turma;
 
+import com.senai.projeto_catraca.model.usuario.aluno.Aluno;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Turmas {
+    private int id;
+    private String nome;
+    private String turno;
+    private int professorId;  // Id do professor responsável
+    private List<Aluno> matriculasAlunos = new ArrayList<>();  // IDs de alunos associados à turma
+
+    public Turmas(String nome, String turno, int professorId) {
+        this.nome = nome;
+        this.turno = turno;
+        this.professorId = professorId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTurno() {
+        return turno;
+    }
+
+    public void setTurno(String turno) {
+        this.turno = turno;
+    }
+
+    public int getProfessorId() {
+        return professorId;
+    }
+
+    public void setProfessorId(int professorId) {
+        this.professorId = professorId;
+    }
+
+    public List<Aluno> getMatriculasAlunos() {
+        return matriculasAlunos;
+    }
+
+    public void setMatriculasAlunos(List<Aluno> matriculasAlunos) {
+        this.matriculasAlunos = matriculasAlunos;
+    }
+
+    public void adicionarAluno(Aluno aluno) {
+        if (!matriculasAlunos.contains(aluno)) {
+            matriculasAlunos.add(aluno);
+        }
+    }
+
+    public void removerAluno(int matricula) {
+        matriculasAlunos.remove(Integer.valueOf(matricula));
+    }
 }
