d43bfe94bc1dc933bb975ae1215dffa0e9e10f65
Allan <allanlealluz@gmail.com>
2025-05-23 08:24:55 -0300

    pequeno commit

--- src/main/java/com/senai/projeto_catraca/model/usuario/Administrador.java
+++ src/main/java/com/senai/projeto_catraca/model/usuario/Administrador.java
@@ -1,7 +1,17 @@
 package com.senai.projeto_catraca.model.usuario;
 
 public class Administrador extends Usuario {
-    public Administrador(String nome, String senha, String CPF, int id, String endereco, String telefone) {
+    private String permissao;
+    public Administrador(String nome, String senha, String CPF, int id, String endereco, String telefone,String permissao) {
         super(nome, senha, CPF, id, endereco, telefone);
+        this.permissao = permissao;
+    }
+
+    public String getPermissao() {
+        return permissao;
+    }
+
+    public void setPermissao(String permissao) {
+        this.permissao = permissao;
     }
 }
