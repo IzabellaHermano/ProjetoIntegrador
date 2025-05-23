512e58dda1701dc612a6fd8cf33664582b8b9c20
Allan <allanlealluz@gmail.com>
2025-05-23 10:06:52 -0300

    Coordenador

--- src/main/java/com/senai/projeto_catraca/model/dao/json/CoordenadorDAO.java
+++ src/main/java/com/senai/projeto_catraca/model/dao/json/CoordenadorDAO.java
@@ -1,4 +1,61 @@
 package com.senai.projeto_catraca.model.dao.json;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.senai.projeto_catraca.model.usuario.Coordenador;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CoordenadorDAO {
+    private final String caminho = "alunos.json";
+    private final Gson gson = new Gson();
+    private final List<Coordenador> coordenadores;
+
+    public CoordenadorDAO() {
+        coordenadores = carregar();
+    }
+
+
+    private List<Coordenador> carregar() {
+        try (FileReader reader = new FileReader(caminho)) {
+            Type listType = new TypeToken<List<Coordenador>>() {}.getType();
+            return gson.fromJson(reader, listType);
+        } catch (IOException e) {
+            return new ArrayList<>();
+        }
+    }
+    private void salvar(List<Coordenador> lista){
+        try (FileWriter writer = new FileWriter(caminho)){
+            gson.toJson(lista, writer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void inserir(Coordenador coordenador) {
+        int novoId = coordenadores.stream().mapToInt(Coordenador::getId).max().orElse(0) + 1;
+        coordenador.setId(novoId);
+        coordenadores.add(coordenador);
+        salvar(coordenadores);
+    }
+    public void atualizar(Coordenador coordenador){
+        for (int i = 0; i < coordenadores.size(); i++) {
+            if (coordenadores.get(i).getId() == coordenador.getId()){
+                coordenadores.set(i, coordenador);
+                break;
+            }
+        }
+        salvar(coordenadores);
+    }
+    public void remover(int id) {
+        coordenadores.removeIf(a -> a.getId() == id);
+        salvar(coordenadores);
+    }
+    public List<Coordenador> listar(){
+        return coordenadores;
+    }
 }
--- src/main/java/com/senai/projeto_catraca/model/usuario/Coordenador.java
+++ src/main/java/com/senai/projeto_catraca/model/usuario/Coordenador.java
@@ -1,4 +1,12 @@
 package com.senai.projeto_catraca.model.usuario;
 
-public class Coordenador{
+public class Coordenador extends Usuario{
+    public Coordenador(String nome, String senha, String CPF, int id, String endereco, String telefone) {
+        super(nome, senha, CPF, id, endereco, telefone);
+    }
+
+    @Override
+    public String getTipo() {
+        return "Coordenador";
+    }
 }
