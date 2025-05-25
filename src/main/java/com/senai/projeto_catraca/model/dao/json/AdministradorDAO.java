fe930d9913e0161f5d1bf1632c188759562165ae
Izabella Hermano <izabellahermano1@gmail.com>
2025-05-25 16:34:49 -0300

    MVC - Classe AQV

--- src/main/java/com/senai/projeto_catraca/model/dao/json/AdministradorDAO.java
+++
@@ -1,4 +0,0 @@
-package com.senai.projeto_catraca.model.dao.json;
-
-public class AdministradorDAO {
-}
--- src/main/java/com/senai/projeto_catraca/model/usuario/AQVDAO.java
+++ src/main/java/com/senai/projeto_catraca/model/usuario/AQVDAO.java
@@ -2,12 +2,15 @@ package com.senai.projeto_catraca.model.usuario;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import com.senai.projeto_catraca.model.usuario.aluno.Aluno;
 
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class AQVDAO {
     private List<AQV> aqvList;
@@ -26,6 +29,39 @@ public class AQVDAO {
         } catch (IOException e) {
             return new ArrayList<>();
         }
+    }
+    private void salvarAQV(List<AQV> aqvList) {
+        try (FileWriter writer = new FileWriter(FILE_PATH)) {
+            gson.toJson(aqvList, writer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void inserir(AQV aqv) {
+        int novoId = aqvList.stream().mapToInt(AQV::getId).max().orElse(0) + 1;
+        aqv.setId(novoId);
+        aqvList.add(aqv);
+        salvarAQV(aqvList);
+    }
+    public void atualizar(AQV aqv) {
+        for (int i = 0; i < aqvList.size(); i++) {
+            if (aqvList.get(i).getId() == aqv.getId()) {
+                aqvList.set(i, aqv);
+                break;
+            }
+        }
+        salvarAQV(aqvList);
+    }
+    public void removerAQV(int id) {
+        aqvList.removeIf(a -> a.getId() == id);
+        salvarAQV(aqvList);
+    }
+    public Optional<AQV> buscarPorId(int id) {
+        return aqvList.stream().filter(a -> a.getId() == id).findFirst();
+    }
+    public List<AQV>listarAQV(){
+        return aqvList;
+
     }
 
 }
