18141e79df16421f96f5c88a022dd97bb682eed4
Allan <allanlealluz@gmail.com>
2025-05-26 09:14:28 -0300

    ocorrencia pt1

--- src/main/java/com/senai/projeto_catraca/model/usuario/aluno/Ocorrencia.java
+++ src/main/java/com/senai/projeto_catraca/model/usuario/aluno/Ocorrencia.java
@@ -4,7 +4,34 @@ public class Ocorrencia {
     private int AlunoID;
     private int JustificativaID;
     private String dataHora;
-    private int TurmaID;
 
+    public Ocorrencia(int alunoID, int justificativaID, String dataHora) {
+        AlunoID = alunoID;
+        JustificativaID = justificativaID;
+        this.dataHora = dataHora;
+    }
 
+    public int getAlunoID() {
+        return AlunoID;
+    }
+
+    public void setAlunoID(int alunoID) {
+        AlunoID = alunoID;
+    }
+
+    public int getJustificativaID() {
+        return JustificativaID;
+    }
+
+    public void setJustificativaID(int justificativaID) {
+        JustificativaID = justificativaID;
+    }
+
+    public String getDataHora() {
+        return dataHora;
+    }
+
+    public void setDataHora(String dataHora) {
+        this.dataHora = dataHora;
+    }
 }
