a11699c91020dee10a9fbb92f31c783fd0085554
Allan <allanlealluz@gmail.com>
2025-06-11 08:37:24 -0300

    ..

--- src/main/java/com/senai/projeto_catraca/model/dao/json/ProfessorDAO.java
+++ src/main/java/com/senai/projeto_catraca/model/dao/json/ProfessorDAO.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 import com.google.gson.Gson;
